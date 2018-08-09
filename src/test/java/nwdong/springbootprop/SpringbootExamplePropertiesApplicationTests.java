cc8093b35d858ed79fcc8db02345beca31d3260d
William Dong <william.dong@nab.com.au>
2018-08-09 16:12:15 +1000

    add assert

--- src/test/java/nwdong/springbootprop/SpringbootExamplePropertiesApplicationTests.java
+++ src/test/java/nwdong/springbootprop/SpringbootExamplePropertiesApplicationTests.java
@@ -1,5 +1,8 @@
 package nwdong.springbootprop;
 
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,19 +47,33 @@ public class SpringbootExamplePropertiesApplicationTests {
 	@Test
 	public void contextLoads() {
 		System.out.println("prop1InSpringBootTest="+prop1InSpringBootTest);
+		assertTrue("prop1-value-in-springboottest".equals(prop1InSpringBootTest));
+		
 		System.out.println("prop2InSpringBootTest="+prop2InSpringBootTest);
+		assertTrue("prop2-value-in-springboottest".equals(prop2InSpringBootTest));
+		
 		System.out.println("prop3InPropertyFile="+prop3InPropertyFile);
+		assertTrue("prop3-value-in-springboottest".equals(prop3InPropertyFile));
+		
 		System.out.println("prop4InPropertyFile="+prop4InPropertyFile);
+		assertTrue("prop4-value-in-springboottest".equals(prop4InPropertyFile));
+		
 		System.out.println("from myclass1="+myClz1.getWhoAmI());
+		assertTrue("I am MyClass1".equals(myClz1.getWhoAmI()));
+		
 		System.out.println("from myclass2="+myClz2.getWhoAmI());
+		assertTrue("I am MyClass2".equals(myClz2.getWhoAmI()));
 		
 		// NULL, it can't be accessed through System.getProperty() but return null instead of throwing exception
 		System.out.println("get prop1InSpringBootTest from System.getProperty(), result= " + System.getProperty("nwdong.springbootprop.prop1-in-springboottest"));
+		assertNull(System.getProperty("nwdong.springbootprop.prop1-in-springboottest"));
 		
 		// NULL, it can't be accessed through System.getProperty() 
 		System.out.println("get prop3InPropertyFile from System.getProperty(), result= " + System.getProperty("nwdong.springbootprop.prop3-in-property-file"));
+		assertNull(System.getProperty("nwdong.springbootprop.prop3-in-property-file"));
 
-		System.out.println("get nwdong.springbootprop.prop5 from System.getProperty(), result= " + System.getProperty("nwdong.springbootprop.prop5"));
+		System.out.println("get nwdong.springbootprop.prop5 from System.getProperty(), result=" + System.getProperty("nwdong.springbootprop.prop5"));
+		assertTrue("prop5-value".equals(System.getProperty("nwdong.springbootprop.prop5")));
 	}
 
 }
